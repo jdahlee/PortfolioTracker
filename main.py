9f150d5f193bba7e38b7b3703ecf07cad4c4dd8e
jdahlee <judahlee29@gmail.com>
2025-09-12 13:38:56 +0900

    Create OllamaClient and pass results to it

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from atproto_client import models
 import os
 from dotenv import load_dotenv
 import threading
+import requests
+import json
+from datetime import datetime, timedelta
 
 load_dotenv()
 
-class PostRetriever:
+class BlueskyClient:
     lock : threading.Lock
     all_posts: list
     client : Client
@@ -21,11 +24,15 @@ class PostRetriever:
         self.client.login(bluesky_username, bluesky_password)
     
     def _retrieve_post(self, search_term : str) -> None:
+        since_str = datetime.now() - timedelta(days=1)
+        print(f'since: {since_str.strftime("%Y-%m-%dT%H:%M:%S.000Z")}')
+
         term_params = models.AppBskyFeedSearchPosts.Params(
             q=search_term,
-            limit=1,
+            since=since_str.strftime("%Y-%m-%dT%H:%M:%S.000Z"),
+            limit=5,
             sort='top',
-            lang='en'
+            lang='en',
         )
         result = self.client.app.bsky.feed.search_posts(
             params=term_params
@@ -35,7 +42,7 @@ class PostRetriever:
             for post in result.posts:
                 self.all_posts.append(post.record)
     
-    def retrieve_all_posts(self) -> None:
+    def retrieve_all_posts(self) -> list:
         threads = []
         search_terms = str(os.getenv('SEARCH_TERMS')).split(',')
         for search_term in search_terms:
@@ -46,38 +53,48 @@ class PostRetriever:
             t.start()
         for t in threads:
             t.join()
+        
+        return self.all_posts
+
+class OllamaClient:
+    url : str
+    model : str
+
+    def __init__(self) -> None:
+        port = os.getenv('OLLAMA_PORT','11434')
+        self.url = 'http://localhost:' + port + '/api/generate'
+        self.model = os.getenv('OLLAMA_MODEL', 'llama3.2')
+
+    def get_post_summary_response(self, posts : list[str]) -> str:
+        prompt = "please summarize the following bluesky posts from the past 24 hours to give an update on the stock market:"
+        prompt += "\n".join(posts)
+        payload = {
+            "model": self.model,
+            "prompt": prompt,
+            "stream": False
+        }
+
+        request_response = requests.post(self.url, json = payload)
+        json_response = json.loads(request_response.text)
+        return json_response["response"]
 
 
 def main():
-    post_retriever = PostRetriever()
-    post_retriever.retrieve_all_posts()
+    bluesky_client = BlueskyClient()
+    all_posts = bluesky_client.retrieve_all_posts()
 
-    for p in post_retriever.all_posts:
-        print("\n" + p.text + "\n")
+    # for p in all_posts:
+    #     print("\n" + p.text + "\n")
+    
+    # TODO clean out posts that contain a link to a personal website
+    post_text = [p.text for p in all_posts]
+    ollama_client = OllamaClient()
+    ollama_summary = ollama_client.get_post_summary_response(post_text)
+    print(ollama_summary)
 
 def test():
-    # bluesky_username = os.getenv('BLUESKY_USERNAME')
-    # bluesky_password = os.getenv('BLUESKY_PASSWORD')
-    
-    # client = Client()
-    # client.login(bluesky_username, bluesky_password)
-
-    # all_posts = []
-    # search_terms = str(os.getenv('SEARCH_TERMS')).split(',')
-
-    # for term in search_terms:
-    #     term_params = models.AppBskyFeedSearchPosts.Params(
-    #         q=term,
-    #         limit=2,
-    #         sort='top',
-    #         lang='en-US'
-    #     )
-    #     result = client.app.bsky.feed.search_posts(
-    #         params=term_params
-    #     )
-    #     for post in result.posts:
-    #         all_posts.append(post.record)
     pass
 
 if __name__ == '__main__':
     main()
+    # test()
