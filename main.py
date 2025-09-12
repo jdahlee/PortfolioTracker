079cbcbb3e9f1aace65d82eb1fcdaeb701909fd3
jdahlee <judahlee29@gmail.com>
2025-09-12 14:16:21 +0900

    Support searching by handle, refactor post retrieval

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ class BlueskyClient:
     lock : threading.Lock
     all_posts: list
     client : Client
+    per_post_retrieval_limit : int
+    since_str : str
 
     def __init__(self) -> None:
         self.lock = threading.Lock()
@@ -22,21 +24,33 @@ class BlueskyClient:
     
         self.client = Client()
         self.client.login(bluesky_username, bluesky_password)
-    
-    def _retrieve_post(self, search_term : str) -> None:
-        since_str = datetime.now() - timedelta(days=1)
-        print(f'since: {since_str.strftime("%Y-%m-%dT%H:%M:%S.000Z")}')
 
-        term_params = models.AppBskyFeedSearchPosts.Params(
+        self.per_post_retrieval_limit = int(os.getenv('PER_POST_RETRIEVAL_LIMIT', '5'))
+        self.since_str = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%S.000Z") # Datetime format expected by Bluesky API
+    
+    def _retrieve_search_term_posts(self, search_term : str) -> None:
+        search_term_params = models.AppBskyFeedSearchPosts.Params(
             q=search_term,
-            since=since_str.strftime("%Y-%m-%dT%H:%M:%S.000Z"),
-            limit=5,
+            limit=self.per_post_retrieval_limit,
+            since=self.since_str,
+            sort='top',
+            lang='en'
+        )
+        self._retrieve_posts(search_term_params)
+
+    def _retrieve_author_handle_posts(self, author_handle : str) -> None:
+        search_term_params = models.AppBskyFeedSearchPosts.Params(
+            q="*",
+            limit=self.per_post_retrieval_limit,
+            since=self.since_str,
             sort='top',
             lang='en',
+            author=author_handle
         )
-        result = self.client.app.bsky.feed.search_posts(
-            params=term_params
-        )
+        self._retrieve_posts(search_term_params)
+
+    def _retrieve_posts(self, params : models.AppBskyFeedSearchPosts.Params) -> None:
+        result = self.client.app.bsky.feed.search_posts(params=params)
 
         with self.lock:
             for post in result.posts:
@@ -44,10 +58,23 @@ class BlueskyClient:
     
     def retrieve_all_posts(self) -> list:
         threads = []
-        search_terms = str(os.getenv('SEARCH_TERMS')).split(',')
-        for search_term in search_terms:
-            t = threading.Thread(target=self._retrieve_post, args=(search_term,))
-            threads.append(t)
+
+        # comma seperated list of terms to search for posts that contain term in body
+        search_terms_str = str(os.getenv('SEARCH_TERMS'))
+        if search_terms_str != None:
+            search_terms = search_terms_str.split(",")
+            for search_term in search_terms:
+                t = threading.Thread(target=self._retrieve_search_term_posts, args=(search_term,))
+                threads.append(t)
+
+        # comma seperated list of author handles to search for posts of, don't include @ ex: joe.bsky.social
+        author_handles_str = str(os.getenv('AUTHOR_HANDLES'))
+        if author_handles_str != None:
+            author_handles = author_handles_str.split(",")
+            for author_handle in author_handles:
+                t = threading.Thread(target=self._retrieve_author_handle_posts, args=(author_handle,))
+                threads.append(t)
+
 
         for t in threads:
             t.start()
